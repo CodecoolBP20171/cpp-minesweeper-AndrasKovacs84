f89cfdd4bc6f8cac7c841f5ff9a9aed5d884dabe
Unknown <ka841001@gmail.com>
2017-10-06 09:50:48 +0200

    Base functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 namespace {
     class Minesweeper {
@@ -23,15 +24,65 @@ namespace {
 
         void countNeighbours() {
             // step 2 goes here
+            char counter;
+            char * pField;
+            bool isAtRightEdge;
+            bool isAtLeftEdge;
+            for (int i = 0; i < (height*width) ; ++i) {
+                counter = '0';
+                isAtRightEdge = (i + 1) % width == 0;
+                isAtLeftEdge = i % width == 0;
+                pField = table+i;
+                if(*pField != MINE){
+                    if(*(pField-width) == MINE) counter++;
+                    if(*(pField-width+1) == MINE && !isAtRightEdge) counter++;
+                    if(*(pField+1) == MINE && !isAtRightEdge) counter++;
+                    if(*(pField+width+1) == MINE && !isAtRightEdge) counter++;
+                    if(*(pField+width) == MINE) counter++;
+                    if(*(pField+width-1) == MINE && !isAtLeftEdge) counter++;
+                    if(*(pField-1) == MINE && !isAtLeftEdge) counter++;
+                    if(*(pField-width-1) == MINE && !isAtLeftEdge) counter++;
+                    *pField = counter;
+                }
+            }
         }
 
         void printTable() const {
             // step 3 goes here
+            std::cout << "\n";
+            for (int j = 0; j < (height*width) ; ++j) {
+                std::cout << *(table + j) << " ";
+                if(0 == (j+1) % width) {
+                    std::cout << "\n";
+                }
+            }
+
         }
 
     private:
+        int totalNrOfMines = 15;
+        const char MINE = '*';
         void fillTable() {
             // step 1 goes here
+            srand(time(0));
+
+            for (int i = 0; i < (width*height); ++i) {
+                *(table + i) = '.';
+            }
+            int placedMines = 0;
+            while(placedMines < totalNrOfMines) {
+                int random = rand() % (width*height);
+                char * pRandomSpot;
+                pRandomSpot = table;
+                //std::cout << "pointer reset to table: " << pRandomSpot << std::endl;
+                if(*(pRandomSpot + random) != MINE) {
+                    //std::cout << *(pRandomSpot + random) << std::endl;
+                    *(pRandomSpot + random) = MINE;
+                    placedMines++;
+                    // std::cout << "placed mines: "<< placedMines << std::endl;
+                }
+                // std::cout << "random: "<< random << std::endl;
+            }
         }
 
         const size_t width, height;
@@ -41,7 +92,7 @@ namespace {
 
 int main() {
     try {
-        Minesweeper ms(100, 50);
+        Minesweeper ms( 10, 10);
         ms.printTable();
         ms.countNeighbours();
         ms.printTable();
